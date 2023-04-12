5dccc13a6b706d06cc1ebf6864cb868b528bca5a
LilasHuez <lilas.huez@gmail.com>
2023-04-12 11:15:10 +0200

    dernière version

---
+++ js/convertisseur.js
@@ -0,0 +1,12 @@
+function convertirEnEuros() {
+    // alert(document.getElementbyId("conversion").value);
+   
+    formInput = document.getElementbyId('conversion');
+    alert ('formInput.value');
+    // = document.getElementbyId('conversion').value * 161.62;
+    
+    // inputValue = formInput.value;
+    
+    // console.log(inputValue);
+  }
+
