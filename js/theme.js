29c3d2d7feb5cb483347e7abda47e535488d3ee6
LilasHuez <lilas.huez@gmail.com>
2023-05-23 17:02:02 +0200

    disco theme

--- js/theme.js
+++ js/theme.js
@@ -1,28 +1,19 @@
-
 const theme = {
-    init: function () {
-        console.log('initialisation du module theme');
-        document.querySelector('#theme-switch').addEventListener('click', function () {
-            
-            if (theme.switchTheme())
-            {
-                document.cookie = 'theme=theme-disco';
-                localStorage.setItem('theme', 'theme-disco');
-            }
-            else 
-            {
-                document.cookie = 'theme=theme-light';
-                localStorage.setItem('theme', 'theme-light');
-            }
-            document.querySelector('.page_title').classList.toggle('theme-disco');
-        });
-       
-    },
-    
-
-    switchTheme : function ()
-    {
-        return document.querySelector('body').classList.toggle('theme-disco');
+    init: function() {
+      console.log('initialisation du module theme');
+      document.querySelector('#theme-switch').addEventListener('click', function() {
+        const bodyElem = document.querySelector('body');
+        bodyElem.classList.toggle('theme-disco');
+        
+        if (bodyElem.classList.contains('theme-disco')) {
+          document.cookie = 'theme=theme-disco';
+          localStorage.setItem('theme', 'theme-disco');
+        } else {
+          document.cookie = 'theme=theme-light';
+          localStorage.setItem('theme', 'theme-light');
+          bodyElem.classList.remove('theme-active');
+        }
+      });
     }
-}
-
+  }
+  
