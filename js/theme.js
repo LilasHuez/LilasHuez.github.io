5428c877d183366290a94ebdfd0880cad5c35a1d
LilasHuez <lilas.huez@gmail.com>
2023-11-18 22:53:19 +0100

    theme-update

--- js/theme.js
+++ js/theme.js
@@ -3,7 +3,7 @@ const theme = {
     // console.log('initialisation du module theme');
     document.querySelector('#theme-switch').addEventListener('click', function() {
       const bodyElem = document.querySelector('body');
-      const bodyHomeElem = document.querySelector('#home');
+      const bodyHomeElem = document.querySelector('body#home');
       bodyElem.classList.toggle('theme-disco');
       bodyHomeElem.classList.toggle('theme-disco');
     
