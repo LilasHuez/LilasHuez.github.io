6623048ca71e79057fc6c75cb3cad02c333d17e6
LilasHuez <lilas.huez@gmail.com>
2023-11-18 22:47:50 +0100

    theme-update

--- js/theme.js
+++ js/theme.js
@@ -1,16 +1,12 @@
 const theme = {
   init: function() {
+    // console.log('initialisation du module theme');
     document.querySelector('#theme-switch').addEventListener('click', function() {
       const bodyElem = document.querySelector('body');
       const bodyHomeElem = document.querySelector('#home');
-
-      // Toggle class on the regular body element
       bodyElem.classList.toggle('theme-disco');
-
-      // Toggle class on the body element with the ID 'home' if it exists
-      if (bodyHomeElem) {
-        bodyHomeElem.classList.toggle('theme-disco');
-      }
+      bodyHomeElem.classList.toggle('theme-disco');
+    
     });
   }
 }
