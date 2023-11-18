5ff3fdcbdec9c9ba05718c139a209e39f2bef0af
LilasHuez <lilas.huez@gmail.com>
2023-11-18 23:03:02 +0100

    theme-update

--- js/theme.js
+++ js/theme.js
@@ -1,16 +1,10 @@
 const theme = {
-  init: function() {
-    document.querySelector('#theme-switch').addEventListener('click', function() {
-      const regularBodyElem = document.querySelector('body');
-      const bodyHomeElem = document.querySelector('body#home');
-
-      // Toggle class on the regular body element
-      regularBodyElem.classList.toggle('theme-disco');
-
-      // Toggle class on the body element with the ID 'home' if it exists
-      if (bodyHomeElem) {
-        bodyHomeElem.classList.toggle('theme-disco');
-      }
-    });
+    init: function() {
+      // console.log('initialisation du module theme');
+      document.querySelector('#theme-switch').addEventListener('click', function() {
+        const bodyElem = document.querySelector('body');
+        bodyElem.classList.toggle('theme-disco');
+      
+      });
+    }
   }
-}
