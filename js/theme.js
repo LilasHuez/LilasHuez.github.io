fd23451432bdc4bae63968f3015df08c9f9cfffd
LilasHuez <lilas.huez@gmail.com>
2024-05-02 03:28:11 +0200

    Upgrades

--- js/theme.js
+++ js/theme.js
@@ -1,10 +1,12 @@
 const theme = {
-    init: function() {
-      // console.log('initialisation du module theme');
-      document.querySelector('#theme-switch').addEventListener('click', function() {
-        const bodyElem = document.querySelector('body');
-        bodyElem.classList.toggle('theme-disco');
-      
-      });
-    }
+  init: function() {
+    // console.log('initialisation du module theme');
+    document.querySelector('#theme-switch').addEventListener('click', function() {
+      const bodyElem = document.querySelector('body');
+      bodyElem.classList.toggle('theme-disco');
+    });
   }
+}
+
+// Initialize the theme module
+theme.init();
